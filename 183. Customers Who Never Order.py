714298ce7a6e48b605cb355dd397af26bf72a732
Utkarsh Shrivastawa <92841647+utkarshshrivastawa@users.noreply.github.com>
2024-04-28 12:37:59 +0530

    Create 183. Customers Who Never Order.py

---
+++ 183. Customers Who Never Order.py
@@ -0,0 +1,83 @@
+# Table: Customers
+
+# +-------------+---------+
+# | Column Name | Type    |
+# +-------------+---------+
+# | id          | int     |
+# | name        | varchar |
+# +-------------+---------+
+# id is the primary key (column with unique values) for this table.
+# Each row of this table indicates the ID and name of a customer.
+ 
+
+# Table: Orders
+
+# +-------------+------+
+# | Column Name | Type |
+# +-------------+------+
+# | id          | int  |
+# | customerId  | int  |
+# +-------------+------+
+# id is the primary key (column with unique values) for this table.
+# customerId is a foreign key (reference columns) of the ID from the Customers table.
+# Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+ 
+
+# Write a solution to find all customers who never order anything.
+
+# Return the result table in any order.
+
+# The result format is in the following example.
+
+ 
+
+# Example 1:
+
+# Input: 
+# Customers table:
+# +----+-------+
+# | id | name  |
+# +----+-------+
+# | 1  | Joe   |
+# | 2  | Henry |
+# | 3  | Sam   |
+# | 4  | Max   |
+# +----+-------+
+# Orders table:
+# +----+------------+
+# | id | customerId |
+# +----+------------+
+# | 1  | 3          |
+# | 2  | 1          |
+# +----+------------+
+# Output: 
+# +-----------+
+# | Customers |
+# +-----------+
+# | Henry     |
+# | Max       |
+# +-----------+
+
+SQL CODE
+# Write your MySQL query statement below
+SELECT name as Customers
+FROM Customers c
+LEFT JOIN Orders o
+ON c.id = o.customerId
+WHERE o.id IS NULL
+
+SELECT name as Customers from Customers 
+where 
+id not in ( 
+    SELECT customerId 
+    from Orders
+)
+
+PYTHON CODE : 
+
+import pandas as pd
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+     df = customers[~customers["id"].isin(orders["customerId"])]
+     df = df[['name']].rename(columns={'name': 'Customers'})
+     return df
